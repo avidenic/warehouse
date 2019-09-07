749227dd1662c05b45b4ab342daf5a71190e20e0
Anže Videnič <anze.videnic@ineor.si>
2019-09-07 10:34:46 +0200

    added client with basic DI wireup

---
+++ src/Client/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
+
+namespace NiceLabel.Demo.Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IServiceCollection _serviceCollection = new ServiceCollection();
+        private readonly IServiceProvider _serviceProvider;
+
+        public App()
+        {
+            ConfigureServices(_serviceCollection);
+            _serviceProvider = _serviceCollection.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _serviceProvider.GetRequiredService<MainWindow>().Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<MainWindow>();
+        }
+    }
+}
